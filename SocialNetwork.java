efcd75ad67d543445ecd35b11ab99d2eb25bff67
hibaakram <47317181+hibaakram@users.noreply.github.com>
2019-03-11 23:15:12 +0500

    Add files via upload

---
+++ SocialNetwork.java
@@ -0,0 +1,112 @@
+/* Copyright (c) 2007-2016 MIT 6.005 course staff, all rights reserved.
+ * Redistribution of original or derived work requires permission of course staff.
+ */
+package twitter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+
+/**
+ * SocialNetwork provides methods that operate on a social network.
+ * 
+ * A social network is represented by a Map<String, Set<String>> where map[A] is
+ * the set of people that person A follows on Twitter, and all people are
+ * represented by their Twitter usernames. Users can't follow themselves. If A
+ * doesn't follow anybody, then map[A] may be the empty set, or A may not even exist
+ * as a key in the map; this is true even if A is followed by other people in the network.
+ * Twitter usernames are not case sensitive, so "ernie" is the same as "ERNie".
+ * A username should appear at most once as a key in the map or in any given
+ * map[A] set.
+ * 
+ * DO NOT change the method signatures and specifications of these methods, but
+ * you should implement their method bodies, and you may add new public or
+ * private methods or classes if you like.
+ */
+public class SocialNetwork {
+
+    /**
+     * Guess who might follow whom, from evidence found in tweets.
+     * 
+     * @param tweets
+     *            a list of tweets providing the evidence, not modified by this
+     *            method.
+     * @return a social network (as defined above) in which Ernie follows Bert
+     *         if and only if there is evidence for it in the given list of
+     *         tweets.
+     *         One kind of evidence that Ernie follows Bert is if Ernie
+     *         @-mentions Bert in a tweet. This must be implemented. Other kinds
+     *         of evidence may be used at the implementor's discretion.
+     *         All the Twitter usernames in the returned social network must be
+     *         either authors or @-mentions in the list of tweets.
+     */
+    public static Map<String, Set<String>> guessFollowsGraph(List<Tweet> tweets) {
+        Map<String, Set<String>> SocialNetwork = new HashMap<String, Set<String>>();
+        List<Tweet> socialtweet = new ArrayList<Tweet>(tweets);
+        for (Tweet tweet: socialtweet){
+            Set<String> blank = new HashSet<String>();
+            Set<String> matchstring = Extract.getMentionedUsers(Arrays.asList(tweet));
+            SocialNetwork.put(tweet.getAuthor().toLowerCase(),matchstring);
+            for (String author: matchstring){
+                if(!author.equals(tweet.getAuthor())){
+                    SocialNetwork.get(tweet.getAuthor().toLowerCase()).add(author.toLowerCase());
+                }
+                }
+            }
+        /**
+        for(Map.Entry<String, Set<String>> entry : SocialNetwork.entrySet()){
+            String key = entry.getKey();
+            if(SocialNetwork.containsValue(key)){
+                SocialNetwork.remove(key);
+            }                        
+         }  */
+        return SocialNetwork;            
+        }
+        //throw new RuntimeException("not implemented");
+
+    /**
+     * Find the people in a social network who have the greatest influence, in
+     * the sense that they have the most followers.
+     * 
+     * @param followsGraph
+     *            a social network (as defined above)
+     * @return a list of all distinct Twitter usernames in followsGraph, in
+     *         descending order of follower count.
+     */
+    public static List<String> influencers(Map<String, Set<String>> followsGraph) {
+        List<String> InfluenceList = new ArrayList<String>();
+        Map<String, Integer> influencers = new HashMap<String, Integer>();
+        int influenceCount;
+        for (Set<String> follows : followsGraph.values()){
+            for(String i: follows){
+                if(influencers.containsKey(i)){
+                    influenceCount = influencers.get(i) + 1;
+                    influencers.put(i.toLowerCase(), influenceCount);
+                }
+                else{
+                    influencers.put(i.toLowerCase(), 1);
+                }
+            }
+        }
+        
+        //sort the influencers and put them in InfluenceList
+        while(!influencers.isEmpty()){
+            int topfollowers = Collections.max(influencers.values());
+            for (String name : influencers.keySet()){
+                if(influencers.get(name).equals(topfollowers)){
+                    InfluenceList.add(name);
+                    influencers.remove(name);
+                    break;
+                }
+            }
+        }
+        return InfluenceList;
+    }
+    
+    }
---
+++ SocialNetworkTest.java
@@ -0,0 +1,110 @@
+/* Copyright (c) 2007-2016 MIT 6.005 course staff, all rights reserved.
+ * Redistribution of original or derived work requires permission of course staff.
+ */
+package twitter;
+
+import static org.junit.Assert.*;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.junit.Test;
+
+public class SocialNetworkTest {
+    
+    private static final Instant d1 = Instant.parse("2016-02-17T10:00:00Z");
+    private static final Instant d2 = Instant.parse("2016-02-17T11:00:00Z");
+    private static final Instant d3 = Instant.parse("2016-02-17T11:25:00Z");
+    private static final Instant d4 = Instant.parse("2016-02-17T11:45:00Z");
+    private static final Instant d5 = Instant.parse("2016-02-17T11:47:00Z");
+    private static final Instant d6 = Instant.parse("2016-02-17T10:47:00Z");
+
+    private static final Tweet tweet1 = new Tweet(1, "Aena", "is it reasonable to talk  ", d1);
+    private static final Tweet tweet2 = new Tweet(2, "BananaCupcake", "can we please pass this test??#hype", d2);
+    private static final Tweet tweet3 = new Tweet(3, "Hiba", "can we please pass this test??", d3);
+    private static final Tweet tweet4 = new Tweet(4, "Imama", "@user1 is this another one of these stupid tests?",
+            d4);
+    private static final Tweet tweet5 = new Tweet(5, "hiraa", "@user1 @user2 Hello world",
+            d5);
+    private static final Tweet tweet6 = new Tweet(6, "hala", "@user1 @user3 I'm doing another test ",
+            d6);
+
+
+    /*
+     * TODO: your testing strategies for these methods should go here.
+     * See the ic03-testing exercise for examples of what a testing strategy comment looks like.
+     * Make sure you have partitions.
+     */
+    
+    @Test(expected=AssertionError.class)
+    public void testAssertionsEnabled() {
+        assert false; // make sure assertions are enabled with VM argument: -ea
+    }
+       
+    
+    @Test
+    public void testGuessFollowsGraphEmpty() {
+        Map<String, Set<String>> followsGraph = SocialNetwork.guessFollowsGraph(new ArrayList<Tweet>());
+        
+        assertTrue("expected empty graph", followsGraph.isEmpty());
+    }
+    
+    @Test
+    public void testInfluencersEmpty() {
+        Map<String, Set<String>> followsGraph = SocialNetwork.guessFollowsGraph(new ArrayList<Tweet>());
+        List<String> influencers = SocialNetwork.influencers(followsGraph);
+        
+        assertTrue("expected empty list", influencers.isEmpty());
+    }
+    
+    
+    @Test
+    public void testGuessFollowsGraphUsernameDifferentCases(){
+        Map<String, Set<String>> followsGraph = SocialNetwork.guessFollowsGraph(Arrays.asList(tweet4, tweet5, tweet6));
+        assertTrue(followsGraph.get("user2").contains("user1"));
+    }
+    
+    @Test
+    public void testInfluencersSixTweets(){
+        Map<String,Set<String>> followsGraph= SocialNetwork.guessFollowsGraph(Arrays.asList(tweet1,tweet2,tweet3,tweet4,tweet5,tweet6));
+        List<String> influencers= SocialNetwork.influencers(followsGraph);
+        assertEquals(influencers.get(0),"user1");
+                  
+        }
+
+    @Test
+    public void testInfluencersSingleTweetnoInfluence(){
+        Map<String,Set<String>> followsGraph= SocialNetwork.guessFollowsGraph(Arrays.asList(tweet1));
+        List<String> influencers= SocialNetwork.influencers(followsGraph);
+        assertEquals(influencers.get(0),"user1");
+                  
+        }
+
+    @Test
+    public void testInfluencersSingleTweetOneInfluence(){
+        Map<String,Set<String>> followsGraph= SocialNetwork.guessFollowsGraph(Arrays.asList(tweet4));
+        List<String> influencers= SocialNetwork.influencers(followsGraph);
+        assertEquals(influencers.get(0),"user1");
+                  
+        }
+
+    /*
+     * Warning: all the tests you write here must be runnable against any
+     * SocialNetwork class that follows the spec. It will be run against several
+     * staff implementations of SocialNetwork, which will be done by overwriting
+     * (temporarily) your version of SocialNetwork with the staff's version.
+     * DO NOT strengthen the spec of SocialNetwork or its methods.
+     * 
+     * In particular, your test cases must not call helper methods of your own
+     * that you have put in SocialNetwork, because that means you're testing a
+     * stronger spec than SocialNetwork says. If you need such helper methods,
+     * define them in a different class. If you only need them in this test
+     * class, then keep them in this test class.
+     */
+
+}
